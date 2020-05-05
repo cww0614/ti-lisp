5faa26e1458c39eb55e9fa698e17f608b093be99
Jianing Li <samosatar@gmail.com>
2020-05-05 19:14:20 -0400

    updated testing framework.

--- testing.ml
+++ testing.ml
@@ -1,45 +1,41 @@
-let _ =
-
+let ls_testcases = [
   (* Test SAST for fib.tlsp. Set the name of your test, inputs, switches,
   expected output, and generated code. Compares if test_output and test_gen
   are the same. *)
-  let test_name = "Test case: (fib.tslp, SAST)" in
-  let test_input = "fib.tlsp" in
-  let test_arg = "-s" in
-  let test_output = "fib.output" in
-  let test_gen = "fib.gen" in
-  let cmd_gen = "./tilisp.native "^test_arg^" tests/"^test_input^" &> tests/"^test_gen in
-  let cmd_compare = "cmp --silent tests/"^test_output^" tests/"^test_gen^
-                    " && echo '"^test_name^" passed' || echo '"^test_name^" failed'"
-  in
-  let _ = Sys.command cmd_gen in
-  let _ = Sys.command cmd_compare 
-  in
+  ("Test case: (fib.tslp, SAST)" , "fib", "-s");
+  
+  (* Test for nested inner defines *)
+  ("Test case: (inner_define_valid.tslp, SAST)", "inner_define_valid", "-s");
 
   (* Test for nested inner defines *)
-  let test_name = "Test case: (inner_define_valid.tslp, SAST)" in
-  let test_input = "inner_define_valid.tlsp" in
-  let test_arg = "-s" in
-  let test_output = "inner_define_valid.output" in
-  let test_gen = "inner_define_valid.gen" in
-  let cmd_gen = "./tilisp.native "^test_arg^" tests/"^test_input^" &> tests/"^test_gen in
-  let cmd_compare = "cmp --silent tests/"^test_output^" tests/"^test_gen^
-                    " && echo '"^test_name^" passed' || echo '"^test_name^" failed'"
-  in
-  let _ = Sys.command cmd_gen in
-  let _ = Sys.command cmd_compare in
+  ("Test case: (inner_define_invalid.tslp, SAST)", "inner_define_invalid", "-s")
+] in
 
-  (* Test for nested inner defines *)
-  let test_name = "Test case: (inner_define_invalid.tslp, SAST)" in
-  let test_input = "inner_define_invalid.tlsp" in
-  let test_arg = "-s" in
-  let test_output = "inner_define_invalid.output" in
-  let test_gen = "inner_define_invalid.gen" in
-  let cmd_gen = "./tilisp.native "^test_arg^" tests/"^test_input^" &> tests/"^test_gen in
-  let cmd_compare = "cmp --silent tests/"^test_output^" tests/"^test_gen^
-                    " && echo '"^test_name^" passed' || echo '"^test_name^" failed'"
-  in
-  let _ = Sys.command cmd_gen in
-  let _ = Sys.command cmd_compare in
+let read_lines ic : string list =
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop [] in
+let compare_list a b =
+  let compare_elem a b = (a=b) in
+  List.for_all2 compare_elem a b in
+let single_test (testcase : (string * string * string)) = 
+  let (title, test_name, test_arg) = testcase in
+  let cmd_run = "./tilisp.native "^test_arg^" tests/"^test_name^".tlsp" in
+  let file_expected = "tests/"^test_name^".output" in
+  let (output, inp, errors) = Unix.open_process_full cmd_run [| |] in
+  let ls_stdout = read_lines output in
+  let ls_errors = read_lines errors in
+  let ls_actual = ls_stdout @ ls_errors in
+  let ic_expected = open_in file_expected in 
+  let ls_expected = read_lines ic_expected in
+  let _ = Unix.close_process_full (output, inp, errors) in
+  if (compare_list ls_actual ls_expected) then
+  print_endline (title^" passed.") else (print_endline (title^" failed.");
+  print_string "Expeceted:\n"; List.iter print_endline ls_expected;
+  print_string "Actual:\n"; List.iter print_endline ls_actual) in
 
-  "Governor Cuomo: we need more testing!"
+List.iter single_test ls_testcases; 
+"Governor Cuomo: we need more testing!"
